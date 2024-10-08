f9a3d674b7004351154e78550f68b87da53c5499
aleks <aleksey.shvoruk@gmail.com>
2024-10-08 20:51:08 +0100

    init commit

---
+++ src/main/java/com/example/sqlreports/SqlreportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlreportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlreportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sqlreports/SqlreportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlreports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlreportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

11689890cd0c4c9d8d9d4b1a1605076eb7e3bcf9
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-12 13:49:11 +0000

    update city repo with new queries

--- src/main/java/com/example/sqlreports/cityReport/CityRepository.java
+++ src/main/java/com/example/sqlreports/cityReport/CityRepository.java
@@ -11,4 +11,16 @@ public interface CityRepository extends CrudRepository <CityEntity, Integer> {
 
     @Query("SELECT Name, Population FROM city ORDER BY Population DESC")
     List<CityEntity> findAllCitiesByWorldOrderedByPopulation();
+
+    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.CountryCode = country.Code WHERE country.Continent = :continent ORDER BY city.Population DESC")
+    List<CityEntity> findAllCitiesByContinentOrderedByPopulation(String continent);
+
+    @Query("SELECT Name, Population FROM city WHERE CountryCode = (SELECT Code FROM country WHERE country.Name = :country)  ORDER BY Population DESC")
+    List<CityEntity> findAllCitiesByCountryOrderedByPopulation(String country);
+
+    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.CountryCode = country.Code WHERE country.Region = :region ORDER BY city.Population DESC")
+    List<CityEntity> findAllCitiesByRegionOrderByPopulation(String region);
+
+    @Query("SELECT Name, Population FROM city WHERE District = :district ORDER BY Population DESC")
+    List<CityEntity> indAllCitiesByDistrictOrderByPopulation(String district);
 }

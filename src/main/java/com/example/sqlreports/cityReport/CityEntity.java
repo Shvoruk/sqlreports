77975af3b1f1c5138cd2d5d6860048a00f778a40
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-12 12:49:49 +0000

    create city entity

---
+++ src/main/java/com/example/sqlreports/cityReport/CityEntity.java
@@ -0,0 +1,53 @@
+package com.example.sqlreports.cityReport;
+
+import org.springframework.data.annotation.Id;
+
+public class CityEntity {
+
+    @Id
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private int population;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}

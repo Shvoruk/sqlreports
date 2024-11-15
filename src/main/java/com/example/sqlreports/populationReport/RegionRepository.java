f2fd64343ed6bd4fd5732c88eaf7da78ea7ba2c3
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-15 19:29:06 +0000

    create region repository for population report

--- src/main/java/com/example/sqlreports/populationReport/RegionRepository.java
+++ src/main/java/com/example/sqlreports/populationReport/RegionRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface RegionRepository extends CrudRepository<Region, Integer> {
 
     @Query("SELECT country.Region, SUM(country.Population) AS total_population, SUM(city.Population) AS city_population, SUM(country.Population) - SUM(city.Population) AS rural_population FROM country LEFT JOIN city ON country.Code = city.CountryCode GROUP BY country.Region")
-    List<Region> findPopulationInRegion();
+    List<Region> findPopulationInRegionFiltered();
 }

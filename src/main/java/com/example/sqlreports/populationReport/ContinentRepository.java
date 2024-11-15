2adc968c0412c7b0ee5df8883be42cbb9b2a4dce
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-15 11:46:33 +0000

    rename repository

--- src/main/java/com/example/sqlreports/populationReport/ContinentRepository.java
+++ src/main/java/com/example/sqlreports/populationReport/ContinentRepository.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PopulationRepository extends CrudRepository<Continent, Integer> {
+public interface ContinentRepository extends CrudRepository<Continent, Integer> {
 
     @Query("SELECT country.Continent, SUM(country.Population) AS total_population, SUM(city.Population) AS city_population, SUM(country.Population) - SUM(city.Population) AS rural_population FROM country LEFT JOIN city ON country.Code = city.CountryCode GROUP BY country.Continent")
     List<Continent> findPopulationInContinent();
-
 }

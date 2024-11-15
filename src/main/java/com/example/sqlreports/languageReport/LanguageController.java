3ae19f700d40ccfb765603ed11bc032d2cd97785
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-15 19:35:55 +0000

    language report finished. Create service and controller

---
+++ src/main/java/com/example/sqlreports/languageReport/LanguageController.java
@@ -0,0 +1,23 @@
+package com.example.sqlreports.languageReport;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/language")
+public class LanguageController {
+
+    private final LanguageService languageService;
+
+    public LanguageController(LanguageService languageService) {
+        this.languageService = languageService;
+    }
+
+    @GetMapping
+    public List<LanguageEntity> getLanguageReport(){
+        return languageService.getLanguageSpeakersPercentage();
+    }
+}

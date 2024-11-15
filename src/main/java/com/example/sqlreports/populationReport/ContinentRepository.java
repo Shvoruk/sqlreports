c384953bc6489096e9e2475c7c51c2e16be49644
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-15 19:28:41 +0000

    create continent repository for population report

--- src/main/java/com/example/sqlreports/populationReport/ContinentRepository.java
+++ src/main/java/com/example/sqlreports/populationReport/ContinentRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 public interface ContinentRepository extends CrudRepository<Continent, Integer> {
 
     @Query("SELECT country.Continent, SUM(country.Population) AS total_population, SUM(city.Population) AS city_population, SUM(country.Population) - SUM(city.Population) AS rural_population FROM country LEFT JOIN city ON country.Code = city.CountryCode GROUP BY country.Continent")
-    List<Continent> findPopulationInContinent();
+    List<Continent> findPopulationInContinentFiltered();
 }

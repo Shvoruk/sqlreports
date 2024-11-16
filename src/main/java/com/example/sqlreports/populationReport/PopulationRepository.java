b004ffde841077760c57c9171016bc0244e03490
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-16 16:14:07 +0000

    update PopulationRepository

--- src/main/java/com/example/sqlreports/populationReport/PopulationRepository.java
+++ src/main/java/com/example/sqlreports/populationReport/PopulationRepository.java
@@ -7,22 +7,22 @@ import java.util.List;
 
 public interface PopulationRepository extends CrudRepository<PopulationEntity, String> {
 
-    @Query("SELECT SUM(Population) AS population FROM country")
+    @Query("SELECT SUM(Population) AS total_population FROM country")
     List<PopulationEntity> findPopulationInWorld();
 
-    @Query("SELECT Continent AS name, SUM(Population) AS population FROM country WHERE Continent = :continent")
+    @Query("SELECT Continent AS name, SUM(Population) AS total_population FROM country WHERE Continent = :continent")
     List<PopulationEntity> findPopulationInContinent(String continent);
 
-    @Query("SELECT Name, Population AS country_population FROM country WHERE Name = :country")
+    @Query("SELECT Name, Population AS total_population FROM country WHERE Name = :country")
     List<PopulationEntity> findPopulationInCountry(String country);
 
-    @Query("SELECT Region AS name, SUM(Population) AS population FROM country WHERE Region = :region")
+    @Query("SELECT Region AS name, SUM(Population) AS total_population FROM country WHERE Region = :region")
     List<PopulationEntity> findPopulationInRegion(String region);
 
-    @Query("SELECT Name, Population AS population FROM city WHERE Name = :city")
+    @Query("SELECT Name, Population AS total_population FROM city WHERE Name = :city")
     List<PopulationEntity> findPopulationInCity(String city);
 
-    @Query("SELECT District AS name, SUM(Population) AS population FROM city WHERE District = :district")
+    @Query("SELECT District AS name, SUM(Population) AS total_population FROM city WHERE District = :district")
     List<PopulationEntity> findPopulationInDistrict(String district);
 
 }

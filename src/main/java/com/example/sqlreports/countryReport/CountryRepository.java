1e871b7ad4c900c1fc98895e367c86a2dc92c332
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-16 14:43:18 +0000

    fix Country repository to select all info

--- src/main/java/com/example/sqlreports/countryReport/CountryRepository.java
+++ src/main/java/com/example/sqlreports/countryReport/CountryRepository.java
@@ -10,21 +10,21 @@ import java.util.List;
 public interface CountryRepository extends CrudRepository <CountryEntity, String> {
 
 
-    @Query("SELECT Name, Population FROM country ORDER BY Population DESC")
+    @Query("SELECT Code, Name, Continent, Region, Population, Capital FROM country ORDER BY Population DESC")
     List<CountryEntity> findAllCountriesByWorldOrderedByPopulation();
 
-    @Query("SELECT Name, Population FROM country WHERE Continent = :continent ORDER BY Population DESC")
+    @Query("SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Continent = :continent ORDER BY Population DESC")
     List<CountryEntity> findCountriesByContinentOrderedByPopulation(String continent);
 
-    @Query("SELECT Name, Population FROM country WHERE Region = :region ORDER BY Population DESC")
+    @Query("SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Region = :region ORDER BY Population DESC")
     List<CountryEntity> findCountriesByRegionOrderedByPopulation(String region);
 
-    @Query("SELECT Name, Population FROM country ORDER BY Population DESC LIMIT :limit")
+    @Query("SELECT Code, Name, Continent, Region, Population, Capital FROM country ORDER BY Population DESC LIMIT :limit")
     List<CountryEntity> findAllCountriesWithLimitByWorldOrderedByPopulation(Integer limit);
 
-    @Query("SELECT Name, Population FROM country WHERE Continent = :continent ORDER BY Population DESC LIMIT :limit")
+    @Query("SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Continent = :continent ORDER BY Population DESC LIMIT :limit")
     List<CountryEntity> findCountriesWithLimitByContinentOrderedByPopulation(String continent, Integer limit);
 
-    @Query("SELECT Name, Population FROM country WHERE Region = :region ORDER BY Population DESC LIMIT :limit")
+    @Query("SELECT Code, Name, Continent, Region, Population, Capital FROM country WHERE Region = :region ORDER BY Population DESC LIMIT :limit")
     List<CountryEntity> findCountriesWithLimitByRegionOrderedByPopulation(String region, Integer limit);
 }

f1d245f3770dbcce3951cfc0b5419f253cc1a762
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-16 19:05:24 +0000

    create capital city entity

---
+++ src/main/java/com/example/sqlreports/capitalCityReport/CapitalCityEntity.java
@@ -0,0 +1,34 @@
+package com.example.sqlreports.capitalCityReport;
+
+public class CapitalCityEntity {
+
+    private String name;
+    private String country;
+    private Integer population;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+}
+

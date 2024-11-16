a8b7065837db1e57e6244397914171e03032c29a
Oleksii <aleksey.shvoruk@gmail.com>
2024-11-16 16:13:37 +0000

    update capital city repository

--- src/main/java/com/example/sqlreports/capitalCityReport/CapitalCityRepository.java
+++ src/main/java/com/example/sqlreports/capitalCityReport/CapitalCityRepository.java
@@ -8,22 +8,22 @@ import java.util.List;
 
 public interface CapitalCityRepository extends CrudRepository<CityEntity, Integer> {
 
-    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.ID = country.Capital ORDER BY city.Population DESC")
+    @Query("SELECT city.Name, city.CountryCode, city.Population FROM city JOIN country ON city.ID = country.Capital ORDER BY city.Population DESC")
     List<CityEntity> findAllCapitalCitiesByWorldOrderedByPopulation();
 
-    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Continent = :continent ORDER BY city.Population DESC")
+    @Query("SELECT city.Name, city.CountryCode, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Continent = :continent ORDER BY city.Population DESC")
     List<CityEntity> findAllCapitalCitiesByContinentOrderedByPopulation(String continent);
 
-    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Region = :region ORDER BY city.Population DESC")
+    @Query("SELECT city.Name, city.CountryCode, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Region = :region ORDER BY city.Population DESC")
     List<CityEntity> findAllCapitalCitiesByRegionOrderedByPopulation(String region);
 
-    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.ID = country.Capital ORDER BY city.Population DESC LIMIT :limit")
+    @Query("SELECT city.Name, city.CountryCode, city.Population FROM city JOIN country ON city.ID = country.Capital ORDER BY city.Population DESC LIMIT :limit")
     List<CityEntity> findAllCapitalCitiesWithLimitByWorldOrderedByPopulation(Integer limit);
 
-    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Continent = :continent ORDER BY city.Population DESC LIMIT :limit")
+    @Query("SELECT city.Name, city.CountryCode, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Continent = :continent ORDER BY city.Population DESC LIMIT :limit")
     List<CityEntity> findAllCapitalCitiesWithLimitByContinentOrderedByPopulation(String continent, Integer limit);
 
-    @Query("SELECT city.Name, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Region = :region ORDER BY city.Population DESC LIMIT :limit")
+    @Query("SELECT city.Name, city.CountryCode, city.Population FROM city JOIN country ON city.ID = country.Capital WHERE country.Region = :region ORDER BY city.Population DESC LIMIT :limit")
     List<CityEntity> findAllCapitalCitiesWithLimitByRegionOrderedByPopulation(String region,Integer limit);
 }
 
